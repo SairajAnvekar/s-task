0a79098bfd9987c73e28cca28e9f0f7b6bdedaac
sairajpersistent <sairaj_anvekar@persistent.co.in>
2017-02-16 16:05:21 +0530

    user sign up and login

---
+++ api/routes/api.js
@@ -0,0 +1,91 @@
+var express = require('express');
+var router = express.Router();
+var Task = require('../../app/models/tasks.js');
+var Sprint = require('../../app/models/sprint.js');
+
+
+
+
+router.get('/',function(req,res,next) {   
+    res.json('home');
+});
+
+
+router.get('/sprint/:id',function(req,res,next) {   
+      Sprint.find({_id: req.params.id},function(err, doc){	 
+	   res.json({"data":doc});       
+	});
+});
+
+
+router.get('/sprint5',function(req,res,next) { 
+	   console.log("eeeeeeeeeeeeeeeeeeeeee")	   
+});
+
+
+router.post('/addTask',function(req, res, next){
+	TaskObj = {          
+	   priority:1,
+	   name:req.body.name,
+	   status:req.body.status,
+	};	
+	
+	var sprintId=req.body.sId
+	Sprint.update({_id:sprintId}, {$push: {tasks: TaskObj}}, {upsert:true}, function(err){
+		if(err){
+				console.log(err);
+		}else{
+				console.log("Successfully added");
+		}
+	});
+
+	Sprint.find({_id:sprintId},function(err, sprint){
+		   res.json({"data":sprint});	
+	});
+
+});
+
+
+
+
+router.post('/addComments',function(req,res){
+	var taskId=req.body._id;
+    var comment=req.body.comment;
+	var user=req.user;
+	  var comments={
+		  comment:comment,
+		  userid:user._id,
+		  userdp:'ffdgg',
+	  };
+	  
+	 	Task.findOneAndUpdate({_id:taskId}, {$push: {comments: comments}}, {upsert:true}, function(err,task){
+			res.json({"data":task});			
+		});
+	 
+	 
+});
+
+router.post('/updateTask',function(req,res){
+	var taskId=req.body._id;
+	var reqTask=req.body.task;
+	
+	Task.findById(reqTask._id, function (err, task) {
+		if (err) return handleError(err);
+         
+		task.status=reqTask.status;
+		task.progress=reqTask.progress;
+		task.priority=reqTask.priority;
+		task.description=reqTask.description;
+		task.asign_to_id=reqTask.asign_to_id;
+		task.save(function (err, updatedTask) {
+		if (err) return handleError(err);
+			res.json({"data":updatedTask});
+		});
+	});
+	console.log(reqTask);
+
+	
+}
+);
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-var express = require('express');
-var mongoose	= 	require('mongoose');
-var app = express();
-
-var Task		= 	require('./app/models/tasks.js');
+var express      = require('express');
+var mongoose     = require('mongoose');
+var app          = express();
+var Task         = require('./app/models/tasks.js');
+var api          = require('./api/routes/api');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+var passport     = require('passport');
+var flash        = require('connect-flash');
 mongoose.connect(process.env.MONGODB||'mongodb://localhost/projectManagement');
+
+
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-
 // views is directory for all template files
 
 engines = require('consolidate');
@@ -18,6 +24,23 @@ app.set('view engine', 'ejs');
 app.use('/node_modules', express.static(__dirname + '/node_modules/'));
 app.set('views', __dirname + '/views');
 app.use('/views', express.static(__dirname + '/views/'));
+
+
+require('./config/passport')(passport); 
+
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+
+app.use(session({
+    secret: 'stask'
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+require('./app/routes/users.js')(app, passport);
+
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
@@ -39,7 +62,7 @@ app.get('/tasks',function(req, res, next){
 
 });
 
-
+app.use('/api/',api);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
